3904926af2126f016dd1371eb2d891a62f22a283
Penneycc <1437987505@qq.com>
2019-08-23 11:07:56 +0800

    登陆页面样式完成

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,13 +1,61 @@
 <template>
     <div class="container">
-
+        <el-card class="mycard">
+            <img src="../../assets/images/logo_index.png" alt="">
+            <el-form :model="Loginform">
+                <el-form-item>
+                    <el-input v-model="Loginform.model" placeholder="请输入手机号"></el-input>
+                </el-form-item>
+                <el-form-item>
+                    <el-input v-model="Loginform.code" placeholder="请输入验证码" style="width: 235px; margin-right: 10px;"></el-input>
+                    <el-button>发送验证码</el-button>
+                </el-form-item>
+                <el-form-item>
+                    <el-checkbox :value="true">备选项</el-checkbox>
+                </el-form-item>
+                <el-form-item>
+                    <el-button type="primary" style="width:100%;">登陆</el-button>
+                </el-form-item>
+            </el-form>
+        </el-card>
     </div>
 </template>
 
 <script>
 export default {
+  data () {
+    return {
+      Loginform: {
+        model: '',
+        code: ''
+      }
+    }
+  }
 }
 </script>
 
 <style lang="less" scoped>
+    .container {
+        width: 100%;
+        height: 100%;
+        position: absolute;
+        left: 0;
+        top: 0;
+        background: url(../../assets/images/login_bg.jpg) no-repeat center / cover;
+
+        .mycard {
+            width: 400px;
+            height: 350px;
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+
+            img {
+                display: block;
+                width: 200px;
+                margin: 0 auto 20px;
+            }
+        }
+    }
 </style>
